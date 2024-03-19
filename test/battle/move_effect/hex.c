467f8745ce69b767747a79a3417840391777ae12
Zimmermann Gyula <graiondilach@hotmail.com>
2024-03-19 19:14:58 +0100

    Fix Hex test.

--- test/battle/move_effect/hex.c
+++ test/battle/move_effect/hex.c
@@ -19,7 +19,7 @@ SINGLE_BATTLE_TEST("Hex deals double damage to foes with a status", s16 damage)
     PARAMETRIZE { status1 = STATUS1_TOXIC_POISON; }
     GIVEN {
         PLAYER(SPECIES_WOBBUFFET);
-        OPPONENT(SPECIES_WOBBUFFET) { Status1(status1); }
+        OPPONENT(SPECIES_WOBBUFFET_TEST) { Status1(status1); }
     } WHEN {
         TURN { MOVE(player, MOVE_HEX); }
     } SCENE {
