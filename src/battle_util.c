47da95ce94f22af9a4dc8f0e8b9eba9d2985bfaf
Zimmermann Gyula <graiondilach@hotmail.com>
2024-04-12 22:01:40 +0200

    Rescale obedience.

--- src/battle_util.c
+++ src/battle_util.c
@@ -8101,22 +8101,22 @@ u8 IsMonDisobedient(void)
         if (FlagGet(FLAG_BADGE08_GET)) // Rain Badge, ignore obedience altogether
             return 0;
 
-        obedienceLevel = 10;
+        obedienceLevel = 15;
 
         if (FlagGet(FLAG_BADGE01_GET)) // Stone Badge
-            obedienceLevel = 20;
-        if (FlagGet(FLAG_BADGE02_GET)) // Knuckle Badge
             obedienceLevel = 30;
+        if (FlagGet(FLAG_BADGE02_GET)) // Knuckle Badge
+            obedienceLevel = 45;
         if (FlagGet(FLAG_BADGE03_GET)) // Dynamo Badge
-            obedienceLevel = 40;
-        if (FlagGet(FLAG_BADGE04_GET)) // Heat Badge
-            obedienceLevel = 50;
-        if (FlagGet(FLAG_BADGE05_GET)) // Balance Badge
             obedienceLevel = 60;
+        if (FlagGet(FLAG_BADGE04_GET)) // Heat Badge
+            obedienceLevel = 75;
+        if (FlagGet(FLAG_BADGE05_GET)) // Balance Badge
+            obedienceLevel = 90;
         if (FlagGet(FLAG_BADGE06_GET)) // Feather Badge
-            obedienceLevel = 70;
+            obedienceLevel = 105;
         if (FlagGet(FLAG_BADGE07_GET)) // Mind Badge
-            obedienceLevel = 80;
+            obedienceLevel = 120;
     }
 
     if (B_OBEDIENCE_MECHANICS >= GEN_8
